df10600717910ab377b7789839982ce992d98ef2
noriva <jrmarson@bol.com.br>
2019-07-11 21:36:38 -0300

    Starting to implement Board and Piece

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,14 +1,13 @@
 package application;
 
+import bordgame.Board;
 import bordgame.Position;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Position pos = new Position(3,5);
-		
-		System.out.println(pos);
+		Board board = new Board(8,8);
 
 	}
 
---
+++ src/bordgame/Piece.java
@@ -0,0 +1,24 @@
+package bordgame;
+
+public class Piece {
+	
+	protected Position position ;
+	private Board board ;
+	
+	public Piece(Board board) {
+		
+		position = null;
+		this.board = board;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+	
+	
+	
+	
+	
+	
+}
