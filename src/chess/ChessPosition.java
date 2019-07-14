3025bbedbac1d237a8038221bda955a336eed9fb
noriva <jrmarson@bol.com.br>
2019-07-14 17:29:16 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,42 @@
+package chess;
+
+import bordgame.BoardException;
+import bordgame.Position;
+
+public class ChessPosition {
+	private char column ;
+	private int row ;
+	public ChessPosition(char column, int row) {
+		
+		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instanciando chessPosition. valid values are from a1 from h8");
+			
+		}
+		
+		this.column = column;
+		this.row = row;
+	}
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	protected Position toPosition () {
+		return new Position(8 - row , column - 'a');
+	}
+	
+	protected static ChessPosition fromPosition (Position position) {
+		return new ChessPosition((char)('a' - position.getCollum()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "" + column + row ;
+	}
+	
+	
+}
